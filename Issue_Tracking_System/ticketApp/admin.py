0e6c9d2c7687c1c59849d830e6920027f0768c61
emukus <mukuna.eugene@gmail.com>
2023-04-06 23:07:44 +0300

    Register admin

---
+++ Issue_Tracking_System/ticketApp/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from .models import Ticket
+
+# Register your models here.
+class TicketAdmin(admin.ModelAdmin):
+    date_hierarchy = 'created_at'
+    list_filter = ('status', 'assignee')
+    list_display = ('id', 'title', 'status', 'assignee',
+            'description', 'updated_at')
+    search_fields = ['title', 'status']
+
+admin.site.register(Ticket, TicketAdmin)

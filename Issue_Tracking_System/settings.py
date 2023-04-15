4add1a95f190944aa889854c91536ce00f955c14
Dovine Owuor <70941806+Dovineowuor@users.noreply.github.com>
2023-04-15 18:53:19 +0300

    settings.py

--- Issue_Tracking_System/settings.py
+++ Issue_Tracking_System/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'Board',
-    'Auth0',
+    'apps.Board',
+    'apps.Auth0',
 ]
 
 MIDDLEWARE = [

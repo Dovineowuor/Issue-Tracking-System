1d909910fb5e518f97c437f3a7d262bda7f5596e
Dovine Owuor <70941806+Dovineowuor@users.noreply.github.com>
2023-04-15 22:38:47 +0300

    Urls file update

--- Issue_Tracking_System/urls.py
+++ Issue_Tracking_System/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from apps.Board import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('Board/', views.board, name='Board'),
 ]

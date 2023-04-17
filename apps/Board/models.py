a26609be0407a375076488850cc280c621554f65
Dovine Owuor <70941806+Dovineowuor@users.noreply.github.com>
2023-04-18 00:19:08 +0300

    models for projects

--- apps/Board/models.py
+++ apps/Board/models.py
@@ -1,7 +1,14 @@
 from django.db import models
 
+
+class Board(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
 # Create your models here.
-# models.py
+models.py
 from django.db import models
 
 class Board(models.Model):

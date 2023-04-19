4ff1996a137979126ab8e639f68ee8db8ca023ca
Dovine Owuor <70941806+Dovineowuor@users.noreply.github.com>
2023-04-20 00:48:57 +0300

    debug

--- apps/Projects/models.py
+++ apps/Projects/models.py
@@ -1,3 +1,42 @@
+from django.db import models
+# from django.contrib.auth.models import User
+# from apps.Projects.models.apps import Project
+from django.db import models
+from django.contrib.auth.models import User
+
+class Project(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    manager = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class Issue(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=20, choices=[('open', 'Open'), ('closed', 'Closed')])
+    priority = models.CharField(max_length=20, choices=[('low', 'Low'), ('medium', 'Medium'), ('high', 'High')])
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    assigned_to = models.ForeignKey(User, on_delete=models.CASCADE, related_name='assigned_issues')
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='created_issues')
+
+class Comment(models.Model):
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    issue = models.ForeignKey(Issue, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class File(models.Model):
+    name = models.CharField(max_length=255)
+    file = models.FileField(upload_to='files/')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    issue = models.ForeignKey(Issue, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
 # # Create your models here.
 # from django.db import models
 # from django.contrib.auth.models import User
@@ -145,41 +184,3 @@
 
 # Updated Version 3
 
-from django.db import models
-# from django.contrib.auth.models import User
-from apps.Projects.models.apps import Project
-from django.db import models
-from django.contrib.auth.models import User
-
-class Project(models.Model):
-    name = models.CharField(max_length=255)
-    description = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    manager = models.ForeignKey(User, on_delete=models.CASCADE)
-
-class Issue(models.Model):
-    title = models.CharField(max_length=255)
-    description = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    status = models.CharField(max_length=20, choices=[('open', 'Open'), ('closed', 'Closed')])
-    priority = models.CharField(max_length=20, choices=[('low', 'Low'), ('medium', 'Medium'), ('high', 'High')])
-    project = models.ForeignKey(Project, on_delete=models.CASCADE)
-    assigned_to = models.ForeignKey(User, on_delete=models.CASCADE, related_name='assigned_issues')
-    created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='created_issues')
-
-class Comment(models.Model):
-    text = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    issue = models.ForeignKey(Issue, on_delete=models.CASCADE)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-
-class File(models.Model):
-    name = models.CharField(max_length=255)
-    file = models.FileField(upload_to='files/')
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    issue = models.ForeignKey(Issue, on_delete=models.CASCADE)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)

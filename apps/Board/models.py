653e7b919b0f57c90a9b1ad789399878f1723987
Dovine Owuor <70941806+Dovineowuor@users.noreply.github.com>
2023-04-19 21:55:47 +0300

    board models

--- apps/Board/models.py
+++ apps/Board/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-
+# Create your models here.
+from django.db import models
 
 class Board(models.Model):
     name = models.CharField(max_length=100)
@@ -7,9 +8,6 @@ class Board(models.Model):
 
     def __str__(self):
         return self.name
-# Create your models here.
-models.py
-from django.db import models
 
 class Board(models.Model):
     name = models.CharField(max_length=255)

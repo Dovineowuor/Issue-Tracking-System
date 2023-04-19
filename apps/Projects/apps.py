dc6317d649b0218c6a52504f860fb9621c6be738
Dovine Owuor <70941806+Dovineowuor@users.noreply.github.com>
2023-04-19 22:00:40 +0300

    projects apps

--- apps/Projects/apps.py
+++ apps/Projects/apps.py
@@ -1,5 +1,24 @@
+# Older version of apps.py
+# from django.apps import AppConfig
+
+# class ProjectsConfig(AppConfig):
+#     default_auto_field = 'django.db.models.BigAutoField'
+#     name = 'apps.Projects'
+
+# from django.apps import AppConfig
+
+# class ProjectsConfig(AppConfig):
+#     default_auto_field = 'django.db.models.BigAutoField'
+#     name = 'apps.Projects'
+
+#     def ready(self):
+#         import apps.Projects.signals
+
 from django.apps import AppConfig
 
 class ProjectsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Projects'
+    name = 'apps.Projects'
+
+    def ready(self):
+        from apps.Projects import signals

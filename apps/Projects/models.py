d2b96d67d198cc02fa0c0774902dc216d1ca6788
Dovine Owuor <70941806+Dovineowuor@users.noreply.github.com>
2023-04-19 17:22:42 +0300

    Project features advancement and updates updates

--- apps/Projects/models.py
+++ apps/Projects/models.py
@@ -1,5 +1,5 @@
-# Create your models here.
 from django.db import models
+# Create your models here.
 from django.contrib.auth.models import User
 
 class Project(models.Model):
@@ -8,6 +8,7 @@ class Project(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
+
 
 class Issue(models.Model):
     title = models.CharField(max_length=255)

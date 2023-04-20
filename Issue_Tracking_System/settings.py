3f1f429e06c7c4cb4801c625b74d0478f324dde2
emukus <mukuna.eugene@gmail.com>
2023-04-20 11:33:54 +0300

    Redis debug toolbar

--- Issue_Tracking_System/settings.py
+++ Issue_Tracking_System/settings.py
@@ -40,9 +40,11 @@ INSTALLED_APPS = [
     'apps.Board',
     'apps.Auth0',
     'apps.Projects',
+    'debug_toolbar',
 ]
 
 MIDDLEWARE = [
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',

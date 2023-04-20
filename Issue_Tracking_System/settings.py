0fdcaafa57825069cd96a9e5aacdcaa2cabcd638
emukus <mukuna.eugene@gmail.com>
2023-04-20 11:18:09 +0300

    Redis cache

--- Issue_Tracking_System/settings.py
+++ Issue_Tracking_System/settings.py
@@ -101,6 +101,13 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+CACHES = {
+    "default": {
+        "BACKEND": "django.core.cache.backends.redis.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379",
+    }
+}
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/

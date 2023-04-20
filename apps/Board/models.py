a24468e9d31abfb084a6c34c53e957d92b4517d7
Dovine Owuor <70941806+Dovineowuor@users.noreply.github.com>
2023-04-20 19:09:12 +0300

    Update db aliases and schema

--- apps/Board/models.py
+++ apps/Board/models.py
@@ -1,17 +1,12 @@
 from django.db import models
-# Create your models here.
-from django.db import models
 
+# Create your Models here
 class Board(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True)
 
     def __str__(self):
         return self.name
-
-class Board(models.Model):
-    name = models.CharField(max_length=255)
-    background = models.ImageField(upload_to='board_backgrounds/', blank=True, null=True)
 
 class List(models.Model):
     board = models.ForeignKey(Board, on_delete=models.CASCADE, related_name='lists')
@@ -24,4 +19,3 @@ class Card(models.Model):
     due_date = models.DateTimeField(blank=True, null=True)
     background = models.ImageField(upload_to='card_backgrounds/', blank=True, null=True)
     pellets = models.ImageField(upload_to='card_pellets/', blank=True, null=True)
-

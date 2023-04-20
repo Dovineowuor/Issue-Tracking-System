2c3032e6bd371a348697d6c13f824af6e3adaf1e
emukus <mukuna.eugene@gmail.com>
2023-04-20 14:32:24 +0300

    Redis

--- Issue_Tracking_System/settings.py
+++ Issue_Tracking_System/settings.py
@@ -110,10 +110,6 @@ CACHES = {
     "default": {
         "BACKEND": "django.core.cache.backends.redis.RedisCache",
         "LOCATION": "redis://127.0.0.1:6379",
-        "OPTIONS": {
-            "CLIENT_CLASS": "django_redis.client.DefaultClient"
-            },
-
     }
 }
 

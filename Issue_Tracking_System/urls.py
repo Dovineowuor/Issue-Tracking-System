d0f56a79496afe166696a27f07bbada826fd76c7
emukus <mukuna.eugene@gmail.com>
2023-04-20 13:01:57 +0300

    Terminate url patterns

--- Issue_Tracking_System/urls.py
+++ Issue_Tracking_System/urls.py
@@ -11,14 +11,12 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('Board/', views.board, name='board'),
     path('Board/', board_list, name='board_list'),
-]   path('Board/create_board/', views.create_board, name='create_board'),
-
+    path('Board/create_board/', views.create_board, name='create_board'),
     path('Board/<int:board_id>/', views.board_detail, name='board_detail'),
-    
     path('Board/<int:board_id>/create_list/', views.create_list, name='create_list'),
-    
     path('Board/<int:list_id>/create_card/', views.create_card, name='create_card'),
     path('__debug__/', include(debug_toolbar.urls)),
+]
 
-    if settings.DEBUG:
+if settings.DEBUG:
         urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT),

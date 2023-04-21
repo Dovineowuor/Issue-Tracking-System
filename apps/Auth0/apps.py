c6d1274705de12e22dcfacaa1f4b9a1a250eb725
Dovine Owuor <70941806+Dovineowuor@users.noreply.github.com>
2023-04-21 23:50:54 +0300

    Refactoring and trimming

--- apps/Auth0/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-class Auth0Config(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'apps.Auth0'
-    label = 'auth0'
-    verbose_name = 'Auth0'

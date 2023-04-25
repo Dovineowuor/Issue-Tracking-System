2f3f1aa3994b483f23864160810222cedc00cdb3
Oluwasefunmi J. Talabi <66198020+Joyce245@users.noreply.github.com>
2023-04-25 01:47:47 +0100

    frontend update

---
+++ dashboard.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>ISSUE TRACKING SYSTEM</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    
+    <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Lora:400,400i,700,700i&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Abril+Fatface&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="css/open-iconic-bootstrap.min.css">
+    <link rel="stylesheet" href="css/animate.css">
+    
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="css/magnific-popup.css">
+
+    <link rel="stylesheet" href="css/aos.css">
+
+    <link rel="stylesheet" href="css/ionicons.min.css">
+
+    <link rel="stylesheet" href="css/bootstrap-datepicker.css">
+    <link rel="stylesheet" href="css/jquery.timepicker.css">
+
+    
+    <link rel="stylesheet" href="css/flaticon.css">
+    <link rel="stylesheet" href="css/icomoon.css">
+    <link rel="stylesheet" href="css/style.css">
+  </head>
+<body>
+
+		<header>
+		<div class="main_menu">
+
+			<nav class="navbar navbar-expand-lg navbar-light">
+				<div class="container">
+					<!-- Brand and toggle get grouped for better mobile display -->
+					<a class="navbar-brand logo_h" href="index.php"><img src="img/logo.png" alt=""></a>
+					
+					<!-- Collect the nav links, forms, and other content for toggling -->
+					<div class="collapse navbar-collapse offset" id="navbarSupportedContent">
+						<ul class="nav navbar-nav menu_nav ml-auto">
+							<li class="nav-item active"><a class="nav-link" href="index.php">TEMPLATES</a></li>
+							<li class="nav-item submenu dropdown active">
+								<a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true"
+								 aria-expanded="false">WORKSPACES</a>
+								<ul class="dropdown-menu">
+									<li class="nav-item"><a class="nav-link" href="courses.php">Issue tracking system</a></li>
+									<li class="nav-item"><a class="nav-link" href="course-details.php">Debugging system</a></li>
+								</ul>
+							</li>
+
+							<li class="nav-item submenu dropdown active">
+								<a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true"
+								 aria-expanded="false">RECENT</a>
+								<ul class="dropdown-menu">
+									<li class="nav-item"><a class="nav-link" href="courses.php">Courses</a></li>
+									<li class="nav-item"><a class="nav-link" href="course-details.php">Course Details</a></li>
+									<li class="nav-item"><a class="nav-link" href="elements.php">Elements</a></li>
+								</ul>
+							</li>
+
+							<li class="nav-item submenu dropdown active">
+								<a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true"
+								 aria-expanded="false">STARRED</a>
+								<ul class="dropdown-menu">
+									<li class="nav-item"><a class="nav-link" href="courses.php">Courses</a></li>
+									<li class="nav-item"><a class="nav-link" href="course-details.php">Course Details</a></li>
+									<li class="nav-item"><a class="nav-link" href="elements.php">Elements</a></li>
+								</ul>
+							</li>
+							<li class="nav-item active"><a class="nav-link" href="contact.php">BLOGS</a></li>
+
+							<!---<li class="nav-item">
+								<a href="#" class="nav-link search" id="search">
+									<i class="lnr lnr-magnifier"></i>
+								</a>
+							</li>-->
+						</ul>
+           
+                        <form class="d-flex justify-content-between" method="" action="">
+						<input type="text" class="form-control" id="search_input" placeholder="Search Here">
+						<button type="submit" class="btn"></button>
+						<span class="lnr lnr-cross" id="close_search" title="Close Search"></span>
+					</form>
+					</div>
+
+				</div>
+			</nav>
+		</div>
+	</header>
+
+
+	<aside id="colorlib-aside" role="complementary" class="js-fullheight">
+      <nav id="colorlib-main-menu" role="navigation">
+        <ul>
+          <li class="colorlib-active"><a href="index.php">Boards</a></li>
+          <li><a href="fashion.php">Members</a></li>
+          <li><a href="post.php">Workspace settings</a></li>
+          <li><a href="about.php">About</a></li>
+
+        </ul>
+      </nav>
+
+      <div class="colorlib-footer">
+        <p class="pfooter">
+    Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved</p>
+      </div>
+    </aside> <!-- END COLORLIB-ASIDE -->
+	<hr>
+</body>
+</html>

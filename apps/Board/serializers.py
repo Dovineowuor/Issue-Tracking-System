6e37c9f607fa1eee2f24e342780156b5d341194b
Dovine Owuor <70941806+Dovineowuor@users.noreply.github.com>
2023-04-25 19:42:18 +0300

    Update serializer

--- apps/Board/serializers.py
+++ apps/Board/serializers.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from rest_framework import serializers
 from .models import Board, List, Card, Issue
 
@@ -11,12 +12,16 @@ class ListSerializer(serializers.ModelSerializer):
         model = List
         fields = '__all__'
 
-class CardSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Card
-        fields = '__all__'
+
 
 class IssueSerializer(serializers.ModelSerializer):
     class Meta:
         model = Issue
+        fields = '__all__'
+        
+class CardSerializer(serializers.ModelSerializer):
+    issues = IssueSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Card
         fields = '__all__'
